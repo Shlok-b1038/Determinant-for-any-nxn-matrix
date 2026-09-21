2d710d871ec947d39e635d81365884458bd622ba
Shlok Bhadani <shlok1038@gmail.com>
2026-09-22 03:36:35 +0530

    Working on an improvement compared to last time

--- input.h
+++ input.h
@@ -28,28 +28,33 @@ int simpleSolver(int *cofactor) {
     return det;
 };
 
-void determinant(int size, int **matrix) {
-    int *reference = matrix[0];
-    int realValue = 0;
-    // for (int i = 0; i < size; i++) {
-    //     printf("%d ", reference[i]);
-    // };
-    int *row = malloc((size - 1)*(size - 1)*sizeof(int));
-
-    for (int k = 0; k < size; k++) {
-        int counter = 0;
-        for (int i = 1; i  < size; i++) {
-            for (int j = 0; j < size; j++) {
-                if (j == k) continue;
-                else {
-                    row[counter] = matrix[i][j];
-                    counter++;
-                };
-            }; 
+void **cofactor(int size, int **matrix, int target) {
+    // int **c_matrix = malloc(size*sizeof(int*));
+    int *data = malloc((size - 1)*(size - 1)*sizeof(int));
+    int counter = 0;
+    for (int i =1; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (j == target) continue;
+            else {
+                data[counter] = matrix[i][j];
+                // printf("%d at %d \n", matrix[i][j], counter);
+                counter++;
+            };
         };
-        int detValue = pow(-1, k)*reference[k]*simpleSolver(row);
-        realValue += detValue;
     };
 
-    printf("Determinant is %d\n", realValue);
+    for (int i = 0; i < (size - 1)*(size - 1); i++) {
+        printf("%d ", data[i]);
+    };
+    printf("\n");
+
+    free(data);
+};
+
+void determinant(int size, int **matrix) {
+    // int *reference = matrix[0];
+    // cofactor(size, matrix, 0);
+    for (int i = 0; i < size; i++) {
+        cofactor(size, matrix, i);
+    };
 };
